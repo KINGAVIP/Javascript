fb70a7f051c96e0a3ad5f82ced1901749b6c2021
KINGAVIP <avipruthi8@gmail.com>
2023-08-21 15:43:03 +0530

    Learnt about objects

--- object.js
+++ object.js
@@ -34,17 +34,23 @@
 // }
 
 // Functions are also objects
-function fn()
-{
-    console.log("Kaisi hai");
-}
-fn.myprop="This is a property withing a fucntion"
-fn.myfunc=function()
-{
-    console.log("This is a method within a function")
-}
-console.log(fn)
-for(key in fn)
-{
-    console.log(key," : ",fn[key])
-}
+// function fn()
+// {
+//     console.log("Kaisi hai");
+// }
+// fn.myprop="This is a property withing a fucntion"
+// fn.myfunc=function()
+// {
+//     console.log("This is a method within a function")
+// }
+// console.log(fn)
+// for(key in fn)
+// {
+//     console.log(key," : ",fn[key])
+// }
+
+// everyhting is object in javascript except the 6 primitive types(atomic and cannot be further broken)
+// number, string, boolean, null, undefined,symbol these are 6 primitive data type
+
+// one non primitive data type -object
+
