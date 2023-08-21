f05a357a3e8b9428adb154c44e11e59bd462cadc
KINGAVIP <avipruthi8@gmail.com>
2023-08-21 12:54:33 +0530

    Data types in javascript

---
+++ object.js
@@ -0,0 +1,50 @@
+// Everything is object in js
+
+// What are objects=>key:value pair
+
+// const object={
+//     "name":"avi",
+//     "2":1,
+//     "age":21,
+//     func:function()
+//     {
+//         console.log("Hi")
+//     }
+// }
+// console.log(object)
+// for(key in object)
+// {
+//     console.log(key," : ",object[key])
+// }
+// object.func()
+
+// const arr=[1,2,3,4,5]
+// console.log(typeof(arr))
+// arr.myname="I am avi";
+// arr.myfunc=function()
+// {
+//     console.log("I am a funky")
+// }
+// arr.push(20)
+// // console.log(arr)
+
+// for (key in arr)
+// {
+//     console.log(key," : ",arr[key])
+// }
+
+// Functions are also objects
+function fn()
+{
+    console.log("Kaisi hai");
+}
+fn.myprop="This is a property withing a fucntion"
+fn.myfunc=function()
+{
+    console.log("This is a method within a function")
+}
+console.log(fn)
+for(key in fn)
+{
+    console.log(key," : ",fn[key])
+}
